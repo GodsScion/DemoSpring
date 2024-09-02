c0a5b65e7d5467634ad647665ea9577ea613e277
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2024-09-01 19:10:42 -0700

    spring app xml config

---
+++ src/main/java/com/springbokka/DemoSpring/App.java
@@ -0,0 +1,15 @@
+package com.springbokka.DemoSpring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext app = new ClassPathXmlApplicationContext("spring.xml");
+    	
+    	Dev dev = app.getBean(Dev.class);
+    	dev.build();
+    }
+}
---
+++ src/main/java/com/springbokka/DemoSpring/Dev.java
@@ -0,0 +1,7 @@
+package com.springbokka.DemoSpring;
+
+public class Dev {
+	public void build() {
+		System.out.println("Building new project");
+	}
+}
